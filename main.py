d97f08d0b6d85afa8631eb4db0c9326e7887b362
lombardi-g <gabriellomba@gmail.com>
2025-04-10 09:24:50 -0300

    refining scrape function

--- main.py
+++ main.py
@@ -5,14 +5,19 @@ from input_interface import url
 response = requests.get(url)
 targetURL = BeautifulSoup(response.text,'html.parser')
 
-ansiedade_list = targetURL.find(id='Ansiedade').find_next().contents
-ansiedade_score = ansiedade_list[4]
-ansiedade_tier = ansiedade_list[8]
-ansiedade = {"score": ansiedade_score, "tier": ansiedade_tier}
-
 def scrape_temper (temper: str):
-    temper_list = targetURL.find(id=f'{temper}').find_next().contents
+    temper_list = targetURL.find(id=temper).find_next().contents
     temper_score = temper_list[4]
     temper_tier = temper_list[8]
-    return {"score": temper_score, "tier": temper_tier}
-
+    match temper:
+        case 'Ansiedade'|'Raiva'|'Melancolia'|'Autoconsciência'|'Impulsividade'|'Vulnerabilidade':
+            greatfive = 'Neurociticismo'
+        case 'Simpatia'|'Simpatia'|'Sociabilidade'|'Assertividade'|'Atividade'|'Busca de Sensações'|'Emoções Positivas':
+            greatfive = 'Extroversão'
+        case 'Fantasia'|'Estética'|'Emotividade'|'Aventura'|'Intelecto'|'Liberalismo':
+            greatfive = 'Abertura a Extroversão'
+        case 'Confiança'|'Moralidade'|'Altruísmo'|'Cooperação'|'Modéstia'|'Empatia':
+            greatfive = 'Afabilidade'
+        case 'Autoeficácia'|'Ordem'|'Senso de Dever'|'Realização-Esforço'|'Autodisciplina'|'Cautela':
+            greatfive = 'Consciência'
+    return [greatfive, temper, temper_score, temper_tier]
